0b4836d5f00627634da2b9038ca92cd4c13c092f
javaczhang@gmail.com <javaczhang@gmail.com>
2013-06-16 20:53:40 +0800

    init

---
+++ src/com/tongda/commonorder/fragment/Home_Fragment.java
@@ -0,0 +1,26 @@
+package com.tongda.commonorder.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tongda.commonorder.R;
+
+public class Home_Fragment extends Fragment{
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		return inflater.inflate(R.layout.home_main, container, false);
+	}
+
+}
